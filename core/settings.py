da699997dbe62c7dc0b6dc019715e01c921b01c9
phillipmugisa <phillipmugisa4@gmail.com>
2022-08-29 07:36:57 +0000

    setup 5

--- core/settings.py
+++ core/settings.py
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
 # django_user_agents
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
         'LOCATION': '127.0.0.1:11211',
     }
 }
